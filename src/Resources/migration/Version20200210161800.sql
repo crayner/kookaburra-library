5d6da51f4327f3a7943a2d3415eb831e4f28165b
Craig Rayner <craig@craigrayner.com>
2020-02-10 16:48:17 +1100

    Module Update Details
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Resources/migration/Version20200210161800.sql
@@ -0,0 +1,2 @@
+INSERT INTO `__prefix__Role` (`category`,`name`,`nameShort`,`description`,`type`,`canLoginRole`,`futureYearsLogin`,`pastYearsLogin`,`restriction`) VALUES
+('Staff','Librarian','LIB','Library Staff','Core','Y','N','N','Admin Only');
